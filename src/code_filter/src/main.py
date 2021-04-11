36fad5ba5ef3c633d8d6712b8cefa07a71dc4a1d
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2021-04-11 18:03:31 +0200

    Add release timer to code filter
    Added a condition to the filter to take into account the time elapsed since the last detection of the code, to avoid issues when the same code is scanned twice but after some minutes

--- src/code_filter/src/main.py
+++ src/code_filter/src/main.py
@@ -1,5 +1,8 @@
 import rospy
 from std_msgs.msg import String
+import time
+
+TIME_THRESHOLD = 30  # 30 seconds before a code is detected twice
 
 
 class CodeFilter():
@@ -8,9 +11,13 @@ class CodeFilter():
     to avoid the same QR code being broadcasted multiple times
     """
 
+    __last_code: str
+    __last_time: time.time
+
     def __init__(self):
         # Initialize the filter
-        self.last_code = None
+        self.__last_code = None
+        self.__last_time = time.time()
         # Create the publisher for the filtered topic
         self.pub = rospy.Publisher("qrcode", String, queue_size=10)
         # Initialize the node
@@ -22,12 +29,19 @@ class CodeFilter():
     def handle_code(self, data):
         received_code = data.data
         # We check whether the received code is the same as the last one
-        if self.last_code is not None and self.last_code == received_code:
-            # If yes we just stop
-            rospy.loginfo("Blocked code %s", received_code)
-            return
+        if self.__last_code is not None and self.__last_code == received_code:
+            # Then check whether the time difference is above the threshold
+            now = time.time()
+            time_diff = now-self.__last_time
+            if(time_diff <= TIME_THRESHOLD):
+                # If yes we update the last time and just stop
+                self.__last_time = now
+                rospy.loginfo("Blocked code %s", received_code)
+
+                return
         # Otherwise we save it as the last emitted code
-        self.last_code = received_code
+        self.__last_code = received_code
+        self.__last_time = time.time()
         # And publish it on the filtered topic
         self.pub.publish(received_code)
 
