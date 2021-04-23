a2a344de29d926780fe3da64b32527bfe9a0b68e
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2021-04-23 12:12:42 +0200

    Implement state pattern to handle incoming QRs in quiz mode

--- controller/phase2/VisitorsController.py
+++ controller/phase2/VisitorsController.py
@@ -1,26 +1,39 @@
-from typing import Optional
+from typing import Optional, Callable
 
-from controller.quiz.QuizController import QuizController
 from controller.BehaviorManager import BehaviorManager
 from controller.phase2.DatabaseManager import DatabaseManager
+from controller.phase2.quiz.QuizController import QuizController
 
 
 class VisitorsController(BehaviorManager):
     __db_manager: DatabaseManager
     # The object controlling the quiz workflow, it can be None if no person is playing
     __quiz_controller: Optional[QuizController]
+    # Function to map the incoming code to the right handler method
+    __state_code_handler: Callable[[str], None]
+
+    def __create_handler_function(self, is_quiz: bool) -> Callable[[str], None]:
+        if not is_quiz:
+            return lambda code: self.__handle_visitor_code(code)
+        else:
+            return lambda code: self.__quiz_controller.handle_code(code)
 
     def __init__(self):
         super().__init__()
         # Instantiate the local database manager
         self.__db_manager = DatabaseManager()
+        # And initialize the idle state
+        self.__state_code_handler = self.__create_handler_function(False)
 
     def handle_person(self, is_person_present: bool) -> None:
         super().handle_person(is_person_present)
 
     def handle_code(self, code_content: str):
         super().handle_code(code_content)
+        self.__state_code_handler(code_content)
 
+    def __handle_visitor_code(self, code_content):
+        print("Received code ", code_content)
         # First we extract the content of the message
         visitor_id = code_content
         # Then we ask the database manager whether the user has already entered or not
@@ -39,12 +52,10 @@ class VisitorsController(BehaviorManager):
 
         # In this last case the user has to participate in the quiz
         self.__db_manager.write_visitor_exit(visitor_id)
-        # We send a message on the quiz topic containing the visitor id
-        # TODO: Instantiate quiz controller and enter in quiz mode
+        # We therefore instantiate the quiz controller
         self.__quiz_controller = QuizController()
-
-        # TODO: Set the state
-
+        # And enter in quiz state
+        self.__state_code_handler = self.__create_handler_function(True)
         # TODO: react and say something
 
     def get_quiz_controller(self) -> QuizController:
